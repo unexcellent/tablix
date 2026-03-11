edf64e8dcb7f3c71fa4404a4e812697601407361
unexcellent <>
2026-03-11 20:26:44 +0100

    feat: implement writing function

--- tablix/renderers/_renderer.py
+++ tablix/renderers/_renderer.py
@@ -1,10 +1,15 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
 
 
 class _Renderer(ABC):
     @abstractmethod
     def lines(self) -> list[str]:
         return NotImplementedError  # type: ignore
+
+    def write_to(self, path: Path | str) -> None:
+        with open(path, "w") as output_file:  # noqa: PTH123
+            output_file.writelines(f"{line}\n" for line in self.lines())
 
     def __str__(self) -> str:
         string = ""
