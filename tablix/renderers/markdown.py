bbb9153e0c458019acaf254e16c668048a839c09
unexcellent <>
2026-03-11 13:13:14 +0100

    feat: markdown bold rendering

--- tablix/renderers/markdown.py
+++ tablix/renderers/markdown.py
@@ -12,23 +12,38 @@ class Markdown:
 
     def lines(self) -> list[str]:
         """Return the string for each line of this table."""
-        column_lengths = self._determine_column_lengths()
-
-        header_row = [field.value.ljust(column_lengths[i]) for i, field in enumerate(self.headers)]
-        separator_row = ["-" * length for length in column_lengths]
-        body_rows = [
-            [field.value.ljust(column_lengths[i]) for i, field in enumerate(row)]
-            for row in self.content
-        ]
-
-        all_rows = [header_row, separator_row, *body_rows]
+        all_rows = _construct_field_strings(self.headers, self.content)
         return [f"| {' | '.join(row)} |" for row in all_rows]
 
-    def _determine_column_lengths(self) -> list[int]:
-        column_lengths = [len(col.value) for col in self.headers]
 
-        for row in self.content:
-            for col, field in enumerate(row):
-                column_lengths[col] = max(column_lengths[col], len(field.value))
+def _construct_field_strings(headers: list[Field], content: list[list[Field]]) -> list[list[str]]:
+    header_strings = [_stringify_format(field) for field in headers]
+    content_strings = [[_stringify_format(field) for field in row] for row in content]
 
-        return column_lengths
+    column_widths = _determine_column_lengths(header_strings, content_strings)
+
+    header_strings = [field.ljust(column_widths[i]) for i, field in enumerate(header_strings)]
+    content_strings = [
+        [field.ljust(column_widths[i]) for i, field in enumerate(row)] for row in content_strings
+    ]
+    separator_strings = ["-" * length for length in column_widths]
+
+    return [header_strings, separator_strings, *content_strings]
+
+
+def _determine_column_lengths(
+    header_strings: list[str], content_strings: list[list[str]]
+) -> list[int]:
+    column_lengths = [len(field) for field in header_strings]
+
+    for row in content_strings:
+        for i, field in enumerate(row):
+            column_lengths[i] = max(column_lengths[i], len(field))
+
+    return column_lengths
+
+
+def _stringify_format(field: Field) -> str:
+    if field.format.bold:
+        return f"**{field.value}**"
+    return field.value
--- test/renderers/test_markdown.py
+++ test/renderers/test_markdown.py
@@ -1,4 +1,4 @@
-from tablix import Table
+from tablix import Table, Format
 
 
 def test_simple():
@@ -8,4 +8,16 @@ def test_simple():
         "| col1   | col2 |",
         "| ------ | ---- |",
         "| value1 | val2 |",
+    ]
+
+
+def test_bold():
+    markdown = Table.from_list(
+        [[("col1", Format(bold=True)), "col2"], ["value1", "val2"]]
+    ).to_markdown()
+
+    assert markdown.lines() == [
+        "| **col1** | col2 |",
+        "| -------- | ---- |",
+        "| value1   | val2 |",
     ]
