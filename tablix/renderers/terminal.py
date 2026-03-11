24d792b0ce3b56d3c97c0f4d803562e2a567e0d2
unexcellent <>
2026-03-11 14:26:19 +0100

    feat: terminal printout bold

--- tablix/renderers/terminal.py
+++ tablix/renderers/terminal.py
@@ -15,50 +15,47 @@ class Terminal(_Renderer):
     def lines(self) -> list[str]:
         """Return the string for each line of this table."""
         lines = []
-        field_strings = _construct_field_strings(self.headers, self.content)
-        for i, row in enumerate(field_strings):
-            lines.append(_add_column_separators(row))
+        fields = _construct_field_strings(self.headers, self.content)
+        for i, row in enumerate(fields):
+            lines.append(_add_column_separators([_stringify_format(field) for field in row]))
 
-            if i == len(field_strings) - 1:
+            if i == len(fields) - 1:
                 continue
 
             separator_type = _SeparatorType.BOLD if i == 0 else _SeparatorType.NORMAL
-            lines.append(_construct_separator_line(row, separator_type))
+            lines.append(_construct_separator_line([field.value for field in row], separator_type))
 
         return lines
 
 
-def _construct_field_strings(headers: list[Field], content: list[list[Field]]) -> list[list[str]]:
-    header_strings = [_stringify_format(field) for field in headers]
-    content_strings = [[_stringify_format(field) for field in row] for row in content]
+def _construct_field_strings(headers: list[Field], content: list[list[Field]]) -> list[list[Field]]:
+    column_widths = _determine_column_lengths(headers, content)
 
-    column_widths = _determine_column_lengths(header_strings, content_strings)
+    for i, field in enumerate(headers):
+        headers[i].value = field.value.ljust(column_widths[i])
 
-    header_strings = [field.ljust(column_widths[i]) for i, field in enumerate(header_strings)]
-    content_strings = [
-        [field.ljust(column_widths[i]) for i, field in enumerate(row)] for row in content_strings
-    ]
+    for r, row in enumerate(content):
+        for c, field in enumerate(row):
+            content[r][c].value = field.value.ljust(column_widths[c])
 
-    return [header_strings, *content_strings]
+    return [headers, *content]
 
 
 def _determine_column_lengths(
-    header_strings: list[str], content_strings: list[list[str]]
+    header_strings: list[Field], content_strings: list[list[Field]]
 ) -> list[int]:
-    column_lengths = [len(field) for field in header_strings]
+    column_lengths = [len(field.value) for field in header_strings]
 
     for row in content_strings:
         for i, field in enumerate(row):
-            column_lengths[i] = max(column_lengths[i], len(field))
+            column_lengths[i] = max(column_lengths[i], len(field.value))
 
     return column_lengths
 
 
 def _stringify_format(field: Field) -> str:
     if field.format.bold:
-        return f"**{field.value}**"
-    if field.format.italic:
-        return f"__{field.value}__"
+        return f"\033[1m{field.value}\033[0m"
     return field.value
 
 
--- test/renderers/test_terminal.py
+++ test/renderers/test_terminal.py
@@ -1,7 +1,7 @@
 from tablix import Table, Format
 
 
-def test_simple_no_bold_headers():
+def test_simple():
     rendered_table = Table.from_list(
         [["col1", "col2"], ["value1", "val2"], ["value3", "val4"]]
     ).to_terminal()
@@ -10,6 +10,20 @@ def test_simple_no_bold_headers():
         "│ col1   │ col2 │",
         "┝━━━━━━━━┿━━━━━━┥",
         "│ value1 │ val2 │",
+        "├────────┼──────┤",
+        "│ value3 │ val4 │",
+    ]
+
+
+def test_bold():
+    rendered_table = Table.from_list(
+        [["col1", "col2"], [("value1", Format(bold=True)), "val2"], ["value3", "val4"]]
+    ).to_terminal()
+
+    assert rendered_table.lines() == [
+        "│ col1   │ col2 │",
+        "┝━━━━━━━━┿━━━━━━┥",
+        "│ \033[1mvalue1\033[0m │ val2 │",  # column width looks weird here but is fine when printing
         "├────────┼──────┤",
         "│ value3 │ val4 │",
     ]
